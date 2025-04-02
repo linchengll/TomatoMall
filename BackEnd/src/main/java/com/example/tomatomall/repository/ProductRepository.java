ff5c8fa9038ab659577da954dff6acf286f97273
UltimatePotato <231250060@smail.nju.edu.cn>
2025-04-02 20:07:29 +0800

    product module:  findAll,findById,updateProduct  specification ID undefined!!

--- BackEnd/src/main/java/com/example/tomatomall/repository/ProductRepository.java
+++ BackEnd/src/main/java/com/example/tomatomall/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.example.tomatomall.po.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
 }
--- BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/ProductServiceImpl.java
+++ BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/ProductServiceImpl.java
@@ -1,28 +1,75 @@
 package com.example.tomatomall.service.serviceImpl;
 
+import com.example.tomatomall.exception.TomatoMallException;
+import com.example.tomatomall.po.Product;
+import com.example.tomatomall.po.ProductSpecification;
+import com.example.tomatomall.repository.ProductRepository;
 import com.example.tomatomall.service.ProductService;
 import com.example.tomatomall.vo.ProductStockpileVO;
 import com.example.tomatomall.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
     @Override
     public List<ProductVO> getProductList() {
-        return Collections.emptyList();
+        List<Product> raw=productRepository.findAll();
+        List<ProductVO> VO=new ArrayList<>();
+        if(raw.isEmpty())
+            return VO;
+        for(Product po:raw)
+            VO.add(po.toVO());
+        return VO;
     }
 
     @Override
     public ProductVO getProductById(String id) {
-        return null;
+        Product product;
+        if(productRepository.findById(new Integer(id)).isPresent())
+            product=productRepository.findById(new Integer(id)).get();
+        else
+            throw TomatoMallException.productNotExists();
+        return product.toVO();
     }
 
     @Override
     public Boolean updateProduct(ProductVO productVO) {
-        return null;
+        Product product;
+        if(productRepository.findById(productVO.getId()).isPresent())
+            product=productRepository.findById(productVO.getId()).get();
+        else
+            throw TomatoMallException.productNotExists();
+        if(productVO.getTitle()!=null)
+            product.setTitle(productVO.getTitle());
+        if(productVO.getPrice()!=null)
+            product.setPrice(productVO.getPrice());
+        if(productVO.getRate()!=null)
+            product.setRate(productVO.getRate());
+        if(productVO.getDescription()!=null)
+            product.setDescription(productVO.getDescription());
+        if(productVO.getCover()!=null)
+            product.setCover(productVO.getCover());
+        if(productVO.getDetail()!=null)
+            product.setDetail(productVO.getDetail());
+        if(productVO.getSpecifications()!=null&&!productVO.getSpecifications().isEmpty()){
+            Set<ProductSpecification> specList=product.getSpecifications();
+            for(ProductSpecification spec:productVO.getSpecifications()){
+                specList.removeIf(original -> spec.getId().equals(original.getId()));//存在相同ID则修改
+                specList.add(spec);//无ID则直接添加
+            }
+            product.setSpecifications(specList);
+        }
+        return true;
     }
 
     @Override
