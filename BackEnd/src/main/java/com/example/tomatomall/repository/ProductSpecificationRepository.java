8810e71902aab8538809ca6e5a014bd42ed9eea1
UltimatePotato <231250060@smail.nju.edu.cn>
2025-04-07 19:10:16 +0800

    使用复合主键，现在允许仅id重复，前端按原方案实现即可 创建时也返回更新后的规格信息。

--- BackEnd/src/main/java/com/example/tomatomall/repository/ProductSpecificationRepository.java
+++ BackEnd/src/main/java/com/example/tomatomall/repository/ProductSpecificationRepository.java
@@ -2,10 +2,14 @@ package com.example.tomatomall.repository;
 
 import com.example.tomatomall.po.ProductSpecification;
 
+import com.example.tomatomall.po.compositKey.SpecificationId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
-public interface ProductSpecificationRepository extends JpaRepository<ProductSpecification,Integer> {
-    Set<ProductSpecification> findByProductId(Integer productId);
+public interface ProductSpecificationRepository extends JpaRepository<ProductSpecification, SpecificationId> {
+    @Query("select ps from ProductSpecification ps where ps.specId.productId = :productId")
+    Set<ProductSpecification> findByProductId(@Param("productId") Integer productId);
 }
