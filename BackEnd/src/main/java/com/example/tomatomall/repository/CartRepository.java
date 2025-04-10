59a8271d438ed90f018a8da96637cbed5d0cfea2
UltimatePotato <231250060@smail.nju.edu.cn>
2025-04-10 16:45:27 +0800

    Cart:update&getAll

--- BackEnd/src/main/java/com/example/tomatomall/repository/CartRepository.java
+++ BackEnd/src/main/java/com/example/tomatomall/repository/CartRepository.java
@@ -3,5 +3,8 @@ package com.example.tomatomall.repository;
 import com.example.tomatomall.po.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+    List<Cart> findByUserId(Integer userId);
 }
