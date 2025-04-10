07e8886a6505a1d62971ddb448d6fdd23b264bec
zwz <231250102@smail.nju.edu.cn>
2025-04-10 16:57:41 +0800

    购物车添加与删除功能实现

--- BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
+++ BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.tomatomall.service.serviceImpl;
 
+import com.example.tomatomall.exception.TomatoMallException;
+import com.example.tomatomall.po.Cart;
+import com.example.tomatomall.po.Product;
 import com.example.tomatomall.repository.CartRepository;
 import com.example.tomatomall.repository.ProductRepository;
 import com.example.tomatomall.repository.ProductSpecificationRepository;
@@ -26,12 +29,44 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public CartVO addCart(String productId, Integer quantity) {
-        return null;
+        Product product;
+        Cart cart =new Cart();
+        CartVO cartVO = new CartVO();
+        if(productRepository.findById(new Integer(productId)).isPresent()) {
+            product = productRepository.findById(new Integer(productId)).get();
+            Integer userId = securityUtil.getCurrentUser().getId();
+            Integer stockpile = productStockpileRepository.findByProductId(new Integer(productId)).getAmount();
+            if(stockpile < quantity) {
+                throw TomatoMallException.cartProductQuantityNotEnough();
+            }
+            cart.setProductId(new Integer(productId));
+            cart.setUserId(userId);
+            cart.setQuantity(quantity);
+            Cart savedCart= cartRepository.save(cart);
+            cartVO.setCartItemId(savedCart.getCartItemId());
+            cartVO.setProductId(product.getId());
+            cartVO.setQuantity(quantity);
+            cartVO.setTitle(product.getTitle());
+            cartVO.setPrice(product.getPrice());
+            cartVO.setDescription(product.getDescription());
+            cartVO.setCover(product.getCover());
+            cartVO.setDetail(product.getDetail());
+        }else
+            throw TomatoMallException.productNotExists();
+
+
+        return cartVO;
     }
 
     @Override
     public String deleteCartItem(String cartItemId) {
-        return "";
+        Cart cart = cartRepository.findByCartItemId(new Integer(cartItemId));
+        if(cart!= null){
+            cartRepository.delete(cart);
+            return "删除成功";
+        }else{
+            throw TomatoMallException.cartProductNotExists();
+        }
     }
 
     @Override
