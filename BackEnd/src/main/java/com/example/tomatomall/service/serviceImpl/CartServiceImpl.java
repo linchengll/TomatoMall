21c45419c6e92e67f78c189d8aa4c7c77b20ab85
UltimatePotato <231250060@smail.nju.edu.cn>
2025-04-10 16:45:27 +0800

    Cart:update&getAll

--- BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
+++ BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.tomatomall.service.serviceImpl;
 
+import com.example.tomatomall.exception.TomatoMallException;
+import com.example.tomatomall.po.Cart;
+import com.example.tomatomall.po.Product;
 import com.example.tomatomall.repository.CartRepository;
 import com.example.tomatomall.repository.ProductRepository;
 import com.example.tomatomall.repository.ProductSpecificationRepository;
@@ -10,6 +13,10 @@ import com.example.tomatomall.vo.CartItemsVO;
 import com.example.tomatomall.vo.CartVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CartServiceImpl implements CartService {
@@ -36,11 +43,46 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public String updateCartItem(String cartItemId, Integer quantity) {
-        return "";
+        Cart cartItem;
+        if(cartRepository.findById(new Integer(cartItemId)).isPresent())
+            cartItem=cartRepository.findById(new Integer(cartItemId)).get();
+        else
+            throw TomatoMallException.cartProductNotExists();
+        if(!Objects.equals(securityUtil.getCurrentUser().getId(), cartItem.getUserId()))
+            throw TomatoMallException.userMismatch();
+        if(!productStockpileRepository.findById(cartItem.getProductId()).isPresent()||quantity-cartItem.getQuantity()<productStockpileRepository.findById(cartItem.getProductId()).get().getAmount())
+            throw TomatoMallException.cartProductQuantityNotEnough();
+        cartItem.setQuantity(quantity);
+        return "修改数量成功";
     }
 
     @Override
     public CartItemsVO getCartList() {
-        return null;
+        CartItemsVO cartList=new CartItemsVO();
+        List<CartVO> items=new ArrayList<>();
+        Integer total = 0;
+        float sum= (float) 0;
+        List<Cart> raw=cartRepository.findByUserId(securityUtil.getCurrentUser().getId());
+        if(!raw.isEmpty())
+            for(Cart item:raw){
+                CartVO VO=new CartVO();
+                if(productRepository.findById(item.getProductId()).isPresent())
+                    throw TomatoMallException.cartProductNotExists();
+                Product product=productRepository.findById(item.getProductId()).get();
+                VO.setCartItemId(item.getCartItemId());
+                VO.setProductId(item.getProductId());
+                VO.setQuantity(item.getQuantity());
+                VO.setTitle(product.getTitle());
+                VO.setPrice(product.getPrice());
+                VO.setDescription(product.getDescription());
+                VO.setCover(product.getCover());
+                VO.setDetail(product.getDetail());
+                sum+=VO.getPrice()*VO.getQuantity();
+                total++;
+            }
+        cartList.setItems(items);
+        cartList.setTotal(total);
+        cartList.setTotalAmount(sum);
+        return cartList;
     }
 }
