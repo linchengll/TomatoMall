0eae0f3a0fe7d0df6d058412586a834c3d6711ae
UltimatePotato <231250060@smail.nju.edu.cn>
2025-05-08 20:03:17 +0800

    implement type module: get TypeList

--- BackEnd/src/main/java/com/example/tomatomall/service/TypeService.java
+++ BackEnd/src/main/java/com/example/tomatomall/service/TypeService.java
@@ -2,7 +2,10 @@ package com.example.tomatomall.service;
 
 import com.example.tomatomall.vo.TypeVO;
 
+import java.util.List;
+
 public interface TypeService {
     TypeVO createType(TypeVO typeVO);
     String deleteType(String typeId);
+    List<TypeVO> getAllType();
 }
