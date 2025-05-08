b71e426c0c19f43af5f04bac9e48ddf63f169c96
UltimatePotato <231250060@smail.nju.edu.cn>
2025-05-08 22:30:40 +0800

    search basics complete, method not optimal

--- BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/SearchServiceImpl.java
+++ BackEnd/src/main/java/com/example/tomatomall/service/serviceImpl/SearchServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.tomatomall.service.serviceImpl;
 
+import com.example.tomatomall.exception.TomatoMallException;
 import com.example.tomatomall.po.Product;
+import com.example.tomatomall.po.ProductTypes;
 import com.example.tomatomall.repository.ProductRepository;
+import com.example.tomatomall.repository.ProductTypeRepository;
+import com.example.tomatomall.repository.TypeRepository;
+import com.example.tomatomall.service.ProductService;
 import com.example.tomatomall.service.SearchService;
 import com.example.tomatomall.vo.FilterVO;
 import com.example.tomatomall.vo.ProductBasicVO;
@@ -10,13 +15,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class SearchServiceImpl implements SearchService {
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    TypeRepository typeRepository;
+    @Autowired
+    ProductTypeRepository productTypeRepository;
+    @Autowired
+    ProductService productService;
 
     @Override
     public List<ProductBasicVO> getTop() {
@@ -29,6 +39,30 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public List<ProductVO> search(FilterVO filterVO) {
-        return Collections.emptyList();
+        List<ProductVO> res=new ArrayList<>();
+        if(filterVO.getType()>0){
+            if(!typeRepository.existsById(filterVO.getType()))
+                throw TomatoMallException.typeNotExists();
+            List<ProductTypes> typeFiltered=productTypeRepository.findByTypeId(filterVO.getType());
+            if (typeFiltered.isEmpty())
+                return res;
+            if (filterVO.getSearchString().isEmpty()){
+                for(ProductTypes item:typeFiltered)
+                    res.add(productService.buildVO(productRepository.findById(item.getProductId()).get()));
+                res.sort((p1, p2) -> p2.getPopularity().compareTo(p1.getPopularity()));
+            }else {
+                for(ProductTypes item:typeFiltered) {
+                    if(productRepository.searchTitle(item.getProductId(), filterVO.getSearchString()).isPresent())
+                        res.add(productService.buildVO(productRepository.searchTitle(item.getProductId(), filterVO.getSearchString()).get()));
+                }
+                //
+            }
+        }else {
+            List<Product> raw=productRepository.findByTitleLike("%"+filterVO.getSearchString()+"%");
+            for(Product item:raw)
+                res.add(productService.buildVO(item));
+            //
+        }
+        return res;
     }
 }
