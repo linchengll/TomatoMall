d7aefe5f9a4543c109995befcb485a7d4bbb3796
UltimatePotato <231250060@smail.nju.edu.cn>
2025-05-16 11:28:31 +0800

    搜索模块完成，实现类型过滤/仅搜索/类型过滤+搜索/获取全部。搜索结果按title相似，description相似，detail相似，热度降序的顺序排序，调用方式见接口文档，无修改

--- BackEnd/src/main/java/com/example/tomatomall/repository/ProductRepository.java
+++ BackEnd/src/main/java/com/example/tomatomall/repository/ProductRepository.java
@@ -8,18 +8,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
       Product findByTitle(String title);
+      List<Product> findByIdIn(List<Integer> productIds);
       @Query("select p from Product p order by p.popularity desc")
       List<Product> getTopProducts(Pageable pageable);
       default List<Product> getTop() {
             Pageable top = PageRequest.of(0, 5);
             return getTopProducts(top);
       }
-      @Query("select p from Product p where p.id=:id and p.title like %:key%")
-      Optional<Product> searchTitle(@Param("id") Integer id, @Param("key") String key);
+      //将多表查询拆成两次查询，效率可能更好？毕竟不用笛卡尔积了
+      //@Query("select p from Product p join fetch ProductTypes pt where pt.typeId = :id and p.id = pt.productId and (p.title like %:key% or p.description like %:key% or p.detail like %:key%) order by case when p.title like %:key% then 0 else 1 end, case when p.description like %:key% then 0 else 1 end, case when p.detail like %:key% then 0 else 1 end, p.popularity desc")
+      //@Query(value = "select p.id,title,price,rate,description,cover,detail,popularity from Product p join Product_Types pt where pt.type_Id = :id and p.id = pt.product_Id and (p.title like :key or p.description like :key or p.detail like :key) order by p.title like :key, p.description like :key, p.detail like :key, p.popularity desc",nativeQuery = true)
+      @Query("select p from Product p where p.id in :id and (p.title like %:key% or p.description like %:key% or p.detail like %:key%) order by case when p.title like %:key% then 0 else 1 end, case when p.description like %:key% then 0 else 1 end, case when p.detail like %:key% then 0 else 1 end, p.popularity desc")
+      List<Product> searchWithType(@Param("id") List<Integer> productIds, @Param("key") String key);
 
-      List<Product> findByTitleLike(String key);
+      @Query("select p from Product p where p.title like %:key% or p.description like %:key% or p.detail like %:key% order by case when p.title like %:key% then 0 else 1 end, case when p.description like %:key% then 0 else 1 end, case when p.detail like %:key% then 0 else 1 end, p.popularity desc")
+      List<Product> searchWithoutType(@Param("key") String key);
 }
